309197b7e6a8f182829cfd786886aeb49ccd1987
Vagan Galstian <vagamagajaga@yandex.ru>
2022-10-24 22:08:52 +0400

    Исправлен предыдущий коммит

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -3,9 +3,9 @@ import UIKit
 final class MovieQuizViewController: UIViewController {
     //MARK: - Outlets
 
-    @IBOutlet weak var movieImage: UIImageView!
-    @IBOutlet weak var movieQuestion: UILabel!
-    @IBOutlet weak var movieCount: UILabel!
+    @IBOutlet private weak var movieImage: UIImageView!
+    @IBOutlet private weak var movieQuestion: UILabel!
+    @IBOutlet private weak var movieCount: UILabel!
     
     // MARK: - Variables
     private var currentQuestionIndex: Int = 0
@@ -14,18 +14,14 @@ final class MovieQuizViewController: UIViewController {
     }
     private var correctAnswers: Int = 0
     
-    
     // MARK: - Actions
     @IBAction private func noButtonClicked(_ sender: UIButton) {
         let givenAnswer = false
         showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
-    
-        
     }
     @IBAction private func yesButtonClicked(_ sender: UIButton) {
         let givenAnswer = true
         showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
-        
     }
     
     // MARK: - Methods
@@ -34,18 +30,13 @@ final class MovieQuizViewController: UIViewController {
         movieImage.layer.borderWidth = 8
         movieImage.layer.cornerRadius = 20
         movieImage.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
-        
         if isCorrect {
             correctAnswers += 1
         }
-        
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-            self.movieImage.layer.masksToBounds = true
             self.movieImage.layer.borderWidth = 0
-            self.movieImage.layer.cornerRadius = 20
             self.showNextQuestionOrResults()
         }
-        
     }
     
     private func convert(model: QuizQuestion) -> QuizStepViewModel {
@@ -62,70 +53,46 @@ final class MovieQuizViewController: UIViewController {
     }
 
     private func showResult(quiz result: QuizResultsViewModel) {
-        
         let alert = UIAlertController(
             title: result.title,
             message: result.text,
             preferredStyle: .alert)
-       
+        
         let action = UIAlertAction(
-            
             title: result.buttonText,
             style: .default,
             handler: { _ in
                 
             self.currentQuestionIndex = 0
-                
             self.showQuestion(quiz: self.convert(model: self.currentQuestion))
         })
         
         alert.addAction(action)
-        
         self.present(alert, animated: true, completion: nil)
     }
     
     private func showNextQuestionOrResults() {
-        
-      if currentQuestionIndex == questions.count - 1 { // - 1 потому что индекс начинается с 0, а длинна массива — с 1
-        // показать результат квиза
+      if currentQuestionIndex == questions.count - 1 {
           let text = "Ваш результат: \(correctAnswers) из 10"
           let resultViewModel: QuizResultsViewModel = QuizResultsViewModel(
             title: "Этот раунд окончен!",
             text: text,
             buttonText: "Сыграть еще раз")
           showResult(quiz: resultViewModel)
-          
+          correctAnswers = 0
       } else {
-          
         currentQuestionIndex += 1
         showQuestion(quiz: convert(model: currentQuestion))
-          
       }
     }
 
     // MARK: - Lifecycle
-
-    
-    
-
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         showQuestion(quiz: convert(model: currentQuestion))
-        
-
-
     }
     
-    
-    
-    
-    
-    
-    
-    
     // MARK: - Состояния
-    
     // для состояния "Вопрос задан"
     private struct QuizStepViewModel {
       let image: UIImage
@@ -148,7 +115,6 @@ final class MovieQuizViewController: UIViewController {
     }
     
     // MARK: - Mock-данные
-    
     private let questions: [QuizQuestion] = [
                 QuizQuestion(
                     image: "The Godfather",
